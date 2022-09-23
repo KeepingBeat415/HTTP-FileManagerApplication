4f142305f6521b00df098966649a238edbf023ed
Ms-BUG <sevenliu18@gmail.com>
2022-09-23 09:39:22 -0400

    get -o

--- A1/httpc.py
+++ A1/httpc.py
@@ -1,50 +1,29 @@
 import re
 import socket
-from tabnanny import verbose
-from urllib import response
 from urllib.parse import urlparse
+import os.path
 
+PORT = "80"
 BUFF_SIZE = 1024
 # 300 Multiple Choices 301 Moved Permanently 302 Found 303 See Other
 REDIRECT_CODE = ["300", "301", "303"]
+
 class Httpc():
 
     def __init__(self):
         self.is_verbose = False
-        self.is_headers = ""
-        #self.get_header = "User-Agent: Concordia-HTTP/1.0\r\n"
+        self.passed_headers = "User-Agent: Concordia-HTTP/1.0\r\n"
+        self.file_name = ""
         pass
 
-    def get_help_info(self, arg):
-        if(arg == "normal"):
-            print('\nhttpc is a curl-like application but supports HTTP protocol only.\n' +
-            'Usage: \n' + 
-            '  httpc command [arguments]\n' + 
-            'The commands are: \n' + 
-            '  get executes a HTTP GET request and prints the response.\n' +
-            '  post executes a HTTP POST request and prints the resonse.\n' +
-            '  help prints this screen.\n')
-        elif(arg == "get"):
-            print('\nusage: httpc get [-v] [-h key:value] URL\n' +
-            'Get executes a HTTP GET request for a given URL.\n' +
-            '  -v Prints the detail of the response such as protocol, status, and headers.\n' +
-            '  -h key:value Associates headers to HTTP Request with the format \'key:value\'.\n')
-        else:
-            print('\nusage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n' +
-            'Post executes a HTTP POST request for a given URL with inline data or from file.\n' +
-            '  -v Prints the detail of the response such as protocol, status, and headers.\n' +
-            '  -h key:value Associates headers to HTTP Request with the format \'key:value\'.\n' +
-            '  -d string Associates an inline data to the body HTTP POST request.\n' +
-            '  -f file Associates the content of a file to the body HTTP POST request\n' +
-            'Either [-d] or [-f] can be used but not both.\n')
 
-    def execute(self, cmd):
+    def execute_curl(self, cmd):
         if("help" in cmd and "get" in cmd):
             self.get_help_info("get")
         elif("help" in cmd and "post" in cmd):
-                self.get_help_info("post")
+            self.get_help_info("post")
         elif("help" in cmd):
-            self.get_help_info("normal")
+            self.get_help_info("none")
         elif(cmd.startswith("get") or cmd.startswith("post")):
             self.http_request(cmd)
         else:
@@ -53,40 +32,38 @@ class Httpc():
     def http_request(self, cmd):
 
         self.is_verbose = True if ("-v" in cmd) else False
-        self.is_headers = "User-Agent: Concordia-HTTP/1.0\r\n" + self.get_headers_value(cmd) if ("-h" in cmd) else "User-Agent: Concordia-HTTP/1.0\r\n"
+
+        if ("-h" in cmd): self.passed_headers +=  self.get_passed_headers_value(cmd) 
+        if ("-o" in cmd): self.file_name = (re.findall(r'(\S+$)', cmd))[0]
 
         url = (re.findall(r'(https?://\S+)', cmd))[0]
         if("'" in url): url = url[:-1]
+
         print("[DEBUG]: url ->", type(url), url)
+
         url = urlparse(url)
         print("[DEBUG]: url parse ->", url)
+
         if(cmd.startswith("get")): self.get_request(url)
 
-        pass
 
     def get_request(self, url):
+
         path_with_param = url.path
         if(url.query): path_with_param += '?' + url.query
-        get_header = ( 
-            "GET "+ path_with_param + " HTTP/1.0\r\n" +
-            self.is_headers + "\r\n" +
-            "Host:" + url.hostname + "\r\n\r\n")
-        self.socket_server(url, get_header)
 
-    # def get_redirect_request(self, location):
-    #     path_with_param = url.path
-    #     if(url.query): path_with_param += '?' + url.query
-    #     get_header = ( 
-    #         "GET "+ path_with_param + " HTTP/1.0\r\n" +
-    #         self.is_headers + "\r\n" +
-    #         "Host:" + url.hostname + "\r\n\r\n")
-    #     self.socket_server(url, get_header)
+        header = ( 
+            "GET "+ path_with_param + " HTTP/1.0\r\n" +
+            self.passed_headers + "\r\n" +
+            "Host:" + url.hostname + "\r\n\r\n")
+        self.socket_server(url, header)
 
     def socket_server(self, url_parsed, request):
  
         client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
         try:
             print("[DEBUG]:", url_parsed)
+
             client_socket.connect((url_parsed.hostname, 80))
             print('[DEBUG]: Socket Connect Success')
 
@@ -96,7 +73,9 @@ class Httpc():
 
             response_parsed = HttpResponse(response.decode("utf-8"))
 
-            self.print_content(response_parsed)
+            if(self.file_name): self.download_response(response_parsed)
+            #else:
+            self.print_response(response_parsed)
 
             if(response_parsed.code in REDIRECT_CODE):
 
@@ -108,23 +87,55 @@ class Httpc():
         finally:  
             client_socket.close()
 
-    def get_headers_value(self, cmd):
+    def get_passed_headers_value(self, cmd):
         values = re.findall(r'(\S+:\S+)', cmd)
         for value in values:
             if("https://" in value or "http://" in value): values.remove(value)
         print("[DEBUG]: Get Headers Value =>", "\r\n".join(values))
         return "\r\n".join(values)
 
-    def print_content(self, response):
-        # lines = content.split("\r\n")
+    def download_response(self, response):
+
+        print("[DEBUG]: Download Response Body into", self.file_name)
+        for line in response.body: print(line)
+
+        file = open(self.file_name, "w") if (os.path.exists(self.file_name)) else open(self.file_name, "a")
+  
+        for line in response.body: file.write(line)
+        
+        file.close()
+
+    def print_response(self, response):
+
         print("[DEBUG]: Received Response. \n")
-        #response = HttpResponse(content)
+
         if(self.is_verbose):
             for line in response.header: print(line)
         for line in response.body: print(line)
 
-    def parse_content(self, content):
-        pass
+
+    def get_help_info(self, arg):
+        if(arg == "post"):
+            print('\nusage: httpc post [-v] [-h key:value] [-d inline-data] [-f file] URL\n' +
+            'Post executes a HTTP POST request for a given URL with inline data or from file.\n' +
+            '  -v Prints the detail of the response such as protocol, status, and headers.\n' +
+            '  -h key:value Associates headers to HTTP Request with the format \'key:value\'.\n' +
+            '  -d string Associates an inline data to the body HTTP POST request.\n' +
+            '  -f file Associates the content of a file to the body HTTP POST request\n' +
+            'Either [-d] or [-f] can be used but not both.\n')
+        elif(arg == "get"):
+            print('\nusage: httpc get [-v] [-h key:value] URL\n' +
+            'Get executes a HTTP GET request for a given URL.\n' +
+            '  -v Prints the detail of the response such as protocol, status, and headers.\n' +
+            '  -h key:value Associates headers to HTTP Request with the format \'key:value\'.\n')
+        else:
+            print('\nhttpc is a curl-like application but supports HTTP protocol only.\n' +
+            'Usage: \n' + 
+            '  httpc command [arguments]\n' + 
+            'The commands are: \n' + 
+            '  get executes a HTTP GET request and prints the response.\n' +
+            '  post executes a HTTP POST request and prints the resonse.\n' +
+            '  help prints this screen.\n')
 class HttpResponse():
 
   def __init__(self, response):
@@ -132,11 +143,10 @@ class HttpResponse():
     self.parseText()
 
   def parseText(self):
+
     texts = self.text.split("\r\n\r\n")
     self.header = texts[0].split("\r\n")
     self.body = texts[1].split("\r\n")
-
-    #infos = lines[0].split(" ")
 
     self.code = self.header[0].split(" ")[1]
     self.status = self.header[0].split(" ")[2]
@@ -149,13 +159,11 @@ class HttpResponse():
     print("[DEBUG]: Redirect to ", self.location)
 
 
-httpc = Httpc()
-
 print("==== Welcome to HTTPC Service ==== \n  Type help to list commands.\n  Press 'Ctrl+C' or Type 'quit' to terminate.")
 # Program Start
 while True:
     cmd = input("\n")
-    if("quit" in cmd):
-         break
-    print("[DEBUG]:",cmd)
-    httpc.execute(cmd)
+    if("quit" in cmd): break
+    #print("[DEBUG]:",cmd)
+    httpc = Httpc()
+    httpc.execute_curl(cmd)
--- A1/test.py
+++ A1/test.py
@@ -1,9 +1,23 @@
 import re
 
-str = "get -h key1:value1 key2:value2 'http://httpbin.org/get?course=networking&assignment=1'"
+# str = "get -h key1:value1 key2:value2 'http://httpbin.org/get?course=networking&assignment=1'"
 
-str = "location: /redirect/1"
+# str = "location: /redirect/1"
 
-values = re.findall(r'(\S+/\S+)', str)
+# values = re.findall(r'(\S+/\S+)', str)
 
-print(values)
+# print(values)
+
+# print('''
+# httpc is a curl-like application but supports HTTP protocol only.
+# Usage:
+#   httpc command [arguments]
+# The commands are:
+#   get executes a HTTP GET request and prints the response.
+#   post executes a HTTP POST request and prints the resonse.
+#   help prints this screen.''')
+
+str = "httpc -v 'http://httpbin.org/get?course=networking&assignment=1' -o hello.txt"
+
+
+print(file_name)
