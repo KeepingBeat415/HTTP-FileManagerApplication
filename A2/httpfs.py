ab5b2f62fef836d2cdfa7d2427b46f5464569a03
Ms-BUG <sevenliu18@gmail.com>
2022-10-21 09:10:56 -0400

    using argparse

--- A2/httpfs.py
+++ A2/httpfs.py
@@ -1,4 +1,5 @@
-import re, sys, json, time, socket, logging, os.path, threading
+from cmath import log
+import re, sys, json, time, socket, logging, os.path, threading, argparse
 from FileManager import FileManager
 
 
@@ -16,32 +17,41 @@ class Httpfs():
         self.disposition = ""
         self.code = ""
         self.status = { "200":"OK", "400":"Bad Request", "401":"Unauthorized", "404":"Not Found", "505":"HTTP Version Not Supported"}
+        logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S', stream=sys.stdout, level=logging.DEBUG)
 
 
     def execute_cmd(self, cmd):
         if(cmd.startswith("httpfs")):
-            # Set Logging Debug or Info
-            if("-v" in cmd): 
-                self.verbose = True
-                logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S', stream=sys.stdout, level=logging.DEBUG)
-            else:
-                self.verbose = False
-                logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S', stream=sys.stdout, level=logging.INFO)
-            # Set Port
-            if("-p" in cmd and re.search(r'-p (\S.+?\S+)', cmd)): 
-                self.port = int(re.findall(r'-p (\S.+?\S+)', cmd)[0])
-            # Set server directory
-            if("-d" in cmd and re.search(r'-d (\S.+?\S+)', cmd)):
-                self.dir_path = re.findall(r'-d (\S.+?\S+)', cmd)[0]
+            
+            try:
+                cmd_parser = argparse.ArgumentParser(prog='httpfs', usage='%(prog)s [-v] [-p PORT] [-d PATH-TO-DIR]', description='Process some integers.')
+
+                cmd_parser.add_argument('-v', dest='verbose', action=argparse.BooleanOptionalAction, help='verbose', default=False)
+                cmd_parser.add_argument('-p', dest='port', help='server socket port', type=int, default=8080)
+                cmd_parser.add_argument('-d', dest='dir_path', help='file manager directory', default='data')
+
+                args = cmd_parser.parse_args(cmd[6:].split()) # remove httpfs
+                logging.debug(f"Parser CMD As --> {args}")
+
+                self.verbose, self.port, self.dir_path = args.verbose, args.port, args.dir_path
+
+                if(self.verbose):
+                    logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s', datefmt='%Y/%m/%d %H:%M:%S', stream=sys.stdout, level=logging.DEBUG)
+
+                # Validated the file directory 
                 is_exist_path = os.path.dirname(os.path.realpath(__file__)) + "/" + self.dir_path
                 if(not os.path.exists(is_exist_path)): 
                     return logging.info("File Manger Server Directory is not exist.")
-            
-            logging.info(f"HTTP File Manager Server Setting: Port - {self.port}, Directory - {self.dir_path}, Verbose - {self.verbose}")
 
-            self.run_server()
+                logging.info(f"HTTP File Manager Server Setting: Port - {self.port}, Directory - {self.dir_path}, Verbose - {self.verbose}")
+
+                self.run_server()
+
+            except:
+                logging.info("[ERROR]: Invalid Command, with UNKNOWN command.")
+
         else:
-            logging.info("\n[ERROR]: Invalid Command.")
+            logging.info("[ERROR]: Invalid Command, command should start with \"httpfs\"")
 
 
     def run_server(self):
